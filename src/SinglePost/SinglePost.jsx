014ec8cb9f720e89e5f7854413c9e47593b5c606
mahin Hossain <mahin121121@gmail.com>
2021-06-08 00:10:48 +0600

    iuds

--- src/SinglePost/SinglePost.jsx
+++ src/SinglePost/SinglePost.jsx
@@ -2,26 +2,26 @@ import React from "react";
 
 export default function single() {
   return (
-    <div className="singlePost">
-      <div className="">
+    <div className="singlepostpage">
+      <div className="spd">
         <img
-          className=""
+          className="singlePostimg"
           src="https://images.unsplash.com/photo-1547039996-61c1135690c0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1797&q=80"
-        />
-
-        <div className="">
-          {" "}
-          <h4 className="">Title</h4>
-          <span className="">music</span>
-          <h6 className="">1 hour ago</h6>
+        />{" "}
+        <div className="singlepostimageitem">
+          <h4 className="singlepostTitle">Title</h4>
+          <i class="fas fa-trash"></i>
+          <i class="fas fa-edit bg-danger"></i>
+          {/* <span className="singlepostCat">music</span>
+            <h6 className="singlepostdate">1 hour ago</h6> */}
         </div>
-        <p className="">
-          Sanctus nonumy et duo dolor stet dolor invidunt. Erat kasd stet dolor
-          eirmod diam stet. Est gubergren sadipscing gubergren at. Sanctus
-          nonumy et duo dolor stet dolor invidunt. Erat kasd stet dolor eirmod
-          diam stet. Est gubergren sadipscing gubergren at.
-        </p>
       </div>
+      <p className="singlePostFesc">
+        Sanctus nonumy et duo dolor stet dolor invidunt. Erat kasd stet dolor
+        eirmod diam stet. Est gubergren sadipscing gubergren at. Sanctus nonumy
+        et duo dolor stet dolor invidunt. Erat kasd stet dolor eirmod diam stet.
+        Est gubergren sadipscing gubergren at.
+      </p>
     </div>
   );
 }
